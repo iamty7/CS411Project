200ebd2e1a835486a69e7e20827ff18a05006dad
iamty <taoyu1994x@gmail.com>
2017-11-11 17:18:52 -0500

    return 10 businesses to the home page

--- mysite/yelp/views.py
+++ mysite/yelp/views.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Business, User
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello, world!")
+	business_list = Business.objects.all()[0:10]
+	context = {'business_list': business_list}
+    return render(request, 'yelp/index.html', context)
