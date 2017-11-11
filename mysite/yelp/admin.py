4ab04a99c28559605c6ad9ff3506fd207d31de0e
iamty <taoyu1994x@gmail.com>
2017-11-11 16:41:08 -0500

    make the Busniess and User modifiable in the admin

--- mysite/yelp/admin.py
+++ mysite/yelp/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Business, User
 # Register your models here.
+
+admin.site.register(Business)
+
+admin.site.register(User)
